baa573ac87e61237ef45f08f3afef980e0df5b83
Mohamed Adel <mohamedadel96e@gmail.com>
2025-08-26 23:20:13 +0300

    Refactor database connection logging and update package.json scripts

--- config/db.js
+++ config/db.js
@@ -6,10 +6,10 @@ const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.DBConnectionString);
     console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`.red.underline.bold);
-    console.error((`Error: ${error.message}`));
     process.exit(1);
   }
 };
+
+module.exports = connectDB;
